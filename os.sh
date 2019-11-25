2ff59030c082061f2c8a7576e7878417c5f2de92
Hamid <mamid1706@hotmail.fr>
2019-11-25 13:49:24 -0800

    Update os.sh
    Removed no relevant comments

--- os.sh
+++ os.sh
@@ -17,8 +17,7 @@ nohup emacs #nohup runs a commands and ignores hangup signals (see help)
 ##PS2: is our 2nd  prompt. It  gets shown when a command is not finished. Type echo "asd and hit enter, the secondary prompt will let you enter more lines until you close the inverted commas.
 ##See PS3, PS4... 
 #II.1. Display current environment variable PS1:
-###It displays username + folder path that could be too long
-###\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$
+###"\[\e]0;\u@\h: \w\a\]${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$"
 echo $PS1
 #II.2. Modify PS1
 export PS1='\[\033[32m\]\u\[\033[00m\]:$ '
