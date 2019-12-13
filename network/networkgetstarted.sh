db51a9ade9c9b0341c3e30690cb32d0677ebad3a
Hamid Gasmi <mamid1706@hotmail.fr>
2019-12-13 13:02:47 -0800

    added commands for NFS

--- network/networkgetstarted.sh
+++ network/networkgetstarted.sh
@@ -1,3 +1,11 @@
-#1. DNS:
-#1.1 Send a DNS request:
+#1. Network File Sytem
+#1.1. Display all the file systems that are currently mounted
+df -k
+#1.2. Mount a new folder
+sudo mkdir /mnt/mymnt
+sudo mount ?
+
+#2. DNS:
+#2.1. Send a DNS request:
 nslookup www.google.com
+
