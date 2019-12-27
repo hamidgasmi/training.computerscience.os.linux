9f8b5267984b8c03fabb1c875a496032401bf639
Hamid Gasmi <mamid1706@hotmail.fr>
2019-12-26 16:48:00 -0800

    Docker installation is added

--- docker/docker.sh
+++ docker/docker.sh
@@ -2,7 +2,8 @@
 #1.1 Uninstall Old Versions of Docker:
 sudo apt-get remove docker docker-engine docker.io
 #1.2 Install the new version of docker
-sudo apt-get remove docker docker-engine docker.io
+sudo apt-get install docker.io #Ubuntu/Debian
+yum install docker # RedHat/CentOS
 #2. Start/stop and automate Docker
 #2.1. Start and Automate Docker
 sudo systemctl start docker
